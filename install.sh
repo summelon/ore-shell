0c1e06f2f83af88c14028d95be8e1c5b17a5ca96
summelon <summelon7@gamil.com>
2020-09-10 14:44:16 +0800

    Update spaceship link, close #2

--- install.sh
+++ install.sh
@@ -69,6 +69,7 @@ cp zshrc ""$HOME"/.zshrc" && \
     sed -i -e "/^export ZSH/c \export ZSH=""$HOME"/.oh-my-zsh"" "$HOME"/.zshrc
     # Spaceship theme
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
     # Auto-Suggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     # Syntax-highlighting
